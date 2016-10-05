04253101e88d3ef9df99010a2bca485f0deb6d02
Александр Некрасов <lxmrrr@gmail.com>
2016-10-05 14:17:53 +0300

    Revert "Revert "10.08""
    This reverts commit 1c74b888ad561c3c340def25f0aeaa6c16d4a130.

--- page-home.php
+++ page-home.php
@@ -14,12 +14,21 @@
 				<div class="alfa-slider__item alfa-slider__item--2">
 					<div class="alfa-slider__content lcw">
 						<div class="alfa-slider__container">
-							<h2 class="alfa-slider__header">Металлоконструкции</h2>
-							<p class="alfa-slider__text">Мы проектируем, производим и поставляем полнокомплектные быстровозводимые здания</p>
-							<a href="<?php echo get_permalink( get_page_by_title('Металлоконструкции')->ID ); ?>" class="alfa-slider__link">Перейти</a>
+							<h2 class="alfa-slider__header">Облицовка фасада здания в цветах вашего бренда</h2>
+							<p class="alfa-slider__text">Предлагайте цвета облицовки склада. Наши фасадные материалы позволят воплотить любое цветовое решение. Отделочные металлические элементы окрашиваются порошковым методом в соответствии с цветовой палитрой, насчитывающей 19 цветов RAL. Раскрасьте фасады сборного ангара в корпоративные цвета!</p>
+							<a href="<?php echo get_permalink( get_page_by_title('Контакты')->ID ); ?>" class="alfa-slider__link">Перейти</a>
 						</div>	
 					</div>									
-				</div>				
+				</div>	
+				<div class="alfa-slider__item alfa-slider__item--3">
+					<div class="alfa-slider__content lcw">
+						<div class="alfa-slider__container">
+							<h2 class="alfa-slider__header">Сельскохозяйственные здания</h2>
+							<p class="alfa-slider__text">Теплые здания животноводческих ферм хранилищ и баз по вашему эскизу.</p>
+							<a href="<?php echo get_permalink( get_page_by_title('Проектирование')->ID ); ?>" class="alfa-slider__link">Перейти</a>
+						</div>	
+					</div>									
+				</div>								
 			</div>
 		</div>		
 	</main>
